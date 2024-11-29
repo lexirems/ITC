9fc096d8d7310401b2957e62314308d4b15fac05
lexiiii <ljbremedios@addu.edu.ph>
2024-11-29 19:32:31 +0800

    Update mathop.js

--- mathop.js
+++ mathop.js
@@ -54,12 +54,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('averageButton').addEventListener('click', calculateAverage);
 });
 
-// Function for toggling navigation (if needed)
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else { 
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
+
